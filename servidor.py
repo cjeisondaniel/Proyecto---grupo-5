78478e6c9df89e1af0414c896d293b1451124902
Jeison Cabrera <cjeisondaniel@gmail.com>
2021-10-13 16:46:24 -0500

    actualiza serivdor y pagina usuario.html

--- servidor.py
+++ servidor.py
@@ -21,6 +21,8 @@ baselogin= [
 def home1():
     return render_template("index.html")
 
+#Al iniciar el servidor reenvia a la página inicial de index
+
 @app.route('/login',methods=['POST','GET'])
 def login():
     if request.method == 'POST':
@@ -32,13 +34,12 @@ def login():
                 #return render_template("usuario.html")
                 break            
         return render_template("registro.html")
-    
+#busca los datos de usuario y contraseña encontrados en la base de datos y los reenvía a la página de usuario o a la página de login en el caso que no los encuentre   
         
 @app.route("/<usr>")
 def user(usr):
     return f"<h1{usr}</h1"    
     
-
 @app.route("/ingresardatos", methods=['POST','GET'])
 def ingresardatos():
     nombre = request.form['usulog']
@@ -50,53 +51,112 @@ def ingresardatos():
             
    """ with sqlite3.connect("employee.db") as con:  
                 cur = con.cursor()  
-                cur.execute("INSERT into Employees (nombre, apellido, cedula,correo,telefono,password) values (?,?,?)",(nombre,apellido,cedula,correo,telefono,password))  
+                cur.execute("INSERT into usuario (nombre, apellido, cedula,correo,telefono,password) values (?,?,?)",(nombre,apellido,cedula,correo,telefono,password))  
                 con.commit()  
                 msg = "Employee successfully Added"  
         except:  
             con.rollback()  
             msg = "We can not add the employee to the list"  
+            return render_template("interfazusuario.html")
         finally:  
             return render_template(# usuario ingresado con éxito" "success.html",msg = msg)  
             con.close()  
     """
+#Ingresa los datos ingresados en el login a la tabla de la base de datos con el nombre de usuario    
     
-@app.route('/registro',methods=['GET','POST'])
-def registro():
-    if request.method=='POST':
-        # Handle POST Request here
-        return render_template('registro.html')
 
 
 @app.route("/dashboardadmin",methods=['GET','POST'])
 def dashboardadmin():
     if request.method=='POST':
-        return render_template('admin.html')
-    return render_template('admin.html')
+         con = sqlite3.connect("employee.db")  
+         con.row_factory = sqlite3.Row  
+         cur = con.cursor()  
+         cur.execute("select * from usuario")  
+         rows = cur.fetchall()  
+         return render_template("admin.html",rows = rows)
+    else:
+        msg = "No se han encontrado ningún dato" 
+        
+#Toma los datos del medico o paciente y los coloca en la interfaz de admin
+
+
+
 
 @app.route('/listadolistas',methods=['GET','POST'])
 def listado_listas():
     if request.method=='POST':
-        return render_template('listadolistas.html')
-    return render_template('listadolistas.html')
+        usuario = request.form['usuarios']
+        con = sqlite3.connect("employee.db")  
+        con.row_factory = sqlite3.Row  
+        cur = con.cursor()  
+        cur.execute("select nombre, fechacita, hora  from lista natural join usuario where usulog = nombre")  
+        rows = cur.fetchall()  
+        return render_template("listadodecitas.html",rows = rows)
+    else:
+        msg = "No hay ningúna cita pendiente" 
+ 
 
-@app.route('/detallecita',methods=['GET','POST'])
+
+
+@app.route('/detallecitamedico',methods=['GET','POST'])
 def detalle_cita():
     if request.method=='POST':
-        return render_template('detalle_cita.html')
-    return render_template('detalle_cita.html')
+        nombre = request.form['usulog']
+        con = sqlite3.connect("employee.db")  
+        con.row_factory = sqlite3.Row  
+        cur = con.cursor()  
+        cur.execute("select nombre, especialidad, turno  from citapaciente natural join usuario where usulog = nombre")  
+        rows = cur.fetchall()  
+        return render_template("detalle_citamedico.html",rows = rows)
+    else:
+        msg = "No se han encontrado citas establecidas" 
+#Se toma de la tabla citapaciente los campos de nombre especialdiad y turno y se los manda a la pagina de detalle decita medico
 
-@app.route('/resultadobus',methods=['GET','POST'])
-def reultado_busquedas():
+@app.route('/detallecitapaciente',methods=['GET','POST'])
+def detalle_cita():
     if request.method=='POST':
-        return render_template('resultadobus.html')
-    return render_template('resultadobus.html')
+        nombre = request.form['usulog']
+        con = sqlite3.connect("employee.db")  
+        con.row_factory = sqlite3.Row  
+        cur = con.cursor()  
+        cur.execute("select nombre, especialidad, turno, resumen  from paciente natural join usuario where usulog = nombre")  
+        rows = cur.fetchall()  
+        return render_template("detalle_citamedico.html",rows = rows)
+    else:
+        msg = "No se han encontrado el usuario " 
+#Se toma de la tabla paciente los campos de nombre especialdiad y turno y se los manda a la pagina de detalle decita medico
 
-@app.route('/perfiluser',methods=['GET','POST'])
-def perfil_user():
+
+
+@app.route('/asignarcita',methods=['GET','POST'])
+def detalle_cita():
     if request.method=='POST':
-        return render_template('usuario.html')
-    return render_template('usuario.html')
+        nombre = request.form['usulog']
+        con = sqlite3.connect("employee.db")  
+        con.row_factory = sqlite3.Row  
+        cur = con.cursor()  
+        cur.execute("select especialidad, paciente from asignarcita where user=nombre")  
+        rows = cur.fetchall()  
+        return render_template("asignarcitamedico.html",rows = rows)
+    else:
+        msg = "No se han encontrado el usuario " 
+#Se toma de la tabla de asignar cita los campos de especialdiad y nombre paciente y se los manda a la pagina de asignarcitamedico
+
+
+
+
+
+@app.route("/perfilusuario")  
+def view():  
+    con = sqlite3.connect("employee.db")  
+    con.row_factory = sqlite3.Row  
+    cur = con.cursor()  
+    cur.execute("select * from usuario")  
+    rows = cur.fetchall()  
+    return render_template("usuario.html",rows = rows)
+
+#Busca los datos que se encuentran en la tabla usuario y los envía a la página usuario.
 
 
 if __name__ == '__main__':
